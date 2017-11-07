f12a08e5573d249933bd9f97a4b0d234b592d994
Jeremiah <jeremiahteee@gmail.com>
2017-11-06 21:18:12 -0500

    type error :( can't figure it out

--- classes.py
+++ classes.py
@@ -14,4 +14,46 @@ class Vector:
             raise TypeError("Cannot add types Vector and %s" % type(other))
         return Vector(self.x * other.x, self.y * other.y)
 
-    
+    # dunder methods to overload add, mul
+    def __add__(self, other):
+        if type(self) != type(other):
+            raise TypeError("Cannot add types Vector and %s" % type(other))
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        if type(self) != type(other):
+            raise TypeError("Cannot add types Vector and %s" % type(other))
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __mul__(self, other):
+        if type(self) != type(other):
+            raise TypeError("Cannot multiply types Vector and %s" % type(other))
+        return self.x * other.x + self.y * other.y
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            raise TypeError("Cannot compare types Vector and %s" % type(other))
+        return self.x == other.x, self.y == other.y
+
+    def __div__(self, num):
+        if not isinstance(num, int):
+            raise TypeError("Cannot divided types Vector and %s" % type(num))
+        return self.x / num.x, self.y / num.y
+
+    def __imul__(self, other):
+        if isinstance(other, int):
+            self.x *= other
+            self.y *= other
+            return self
+        else:
+            return self.x * other.x + self.y * other.y
+
+    def __str__(self):
+        return '(%g %g)' % (self.x, self.y)
+
+vec = Vector(1,2)
+print(str(vec))
+print(str(vec.add(3,4)))
+print(str(vec.mul(Vector(3,4))))
