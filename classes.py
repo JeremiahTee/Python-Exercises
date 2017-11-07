e0afe394e531dc619bcbfd7f0a093cd469a27124
Jeremiah <jeremiahteee@gmail.com>
2017-11-07 09:48:48 -0500

    fixed bugs, added more dunder methods

--- classes.py
+++ classes.py
@@ -11,14 +11,14 @@ class Vector:
 
     def mul(self, other):
         if type(self) != type(other):
-            raise TypeError("Cannot add types Vector and %s" % type(other))
+            raise TypeError("Cannot multiply types Vector and %s" % type(other))
         return Vector(self.x * other.x, self.y * other.y)
 
     # dunder methods to overload add, mul
     def __add__(self, other):
         if type(self) != type(other):
             raise TypeError("Cannot add types Vector and %s" % type(other))
-        return Vector(self.x + other.x, self.y + other.y)
+        return self.x + other.x, self.y + other.y
 
     def __iadd__(self, other):
         if type(self) != type(other):
@@ -42,7 +42,7 @@ class Vector:
             raise TypeError("Cannot divided types Vector and %s" % type(num))
         return self.x / num.x, self.y / num.y
 
-    def __imul__(self, other):
+    def __mul__(self, other):
         if isinstance(other, int):
             self.x *= other
             self.y *= other
@@ -51,9 +51,13 @@ class Vector:
             return self.x * other.x + self.y * other.y
 
     def __str__(self):
-        return '(%g %g)' % (self.x, self.y)
+        return '(%g,%g)' % (self.x, self.y)
+
+    def __repr__(self):
+        return ("Vector({%g},{%g})" % (self.x, self.y))
 
 vec = Vector(1,2)
 print(str(vec))
-print(str(vec.add(3,4)))
+print(str(vec.add(Vector(1,1))))
 print(str(vec.mul(Vector(3,4))))
+print(str(vec.__mul__(2)))
